48c968cf70817b4548b4cbf7ac64f1559fa782eb
Никита Алфёров <nnikitka12042006@gmail.com>
2023-10-24 19:25:21 +0300

    Добавьте файлы проекта.

---
+++ ugadaika/Game.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace ugadaika
+{
+    public partial class Game : Window
+    {
+        private int targetNumber;
+        private int attemptsCount;
+
+        DispatcherTimer _timer;
+        TimeSpan _time;
+
+        public Game()// закинуть таймер в функцию и сделать сброс времени после победы или поражение,добавить поражение ну и по визуалу
+        {
+            InitializeComponent();
+
+            _time = TimeSpan.FromSeconds(80);
+
+            _timer = new DispatcherTimer(new TimeSpan(0, 0, 1), DispatcherPriority.Normal, delegate
+            {
+                Time.Text = _time.ToString("c");
+                if (_time == TimeSpan.Zero) Close();
+                _time = _time.Add(TimeSpan.FromSeconds(-1));
+            }, Application.Current.Dispatcher);
+
+            _timer.Start();
+        }
+
+
+        private void StartNewGame()
+        {
+            Random random = new Random();
+            targetNumber = random.Next(0, 10);
+            attemptsCount = 0;
+        }
+
+        private void HandleGuess()
+        {
+            int userGuess;
+
+            if (int.TryParse(guessNumberTextBox.Text, out userGuess))
+            {
+                attemptsCount++;
+
+                if (userGuess == targetNumber)
+                {
+                    resultTextBlock.Text = $"Вы угадали число за {attemptsCount} попыток!";
+                    StartNewGame();
+                }
+                else if (userGuess < targetNumber)
+                {
+                    resultTextBlock.Text = "Загаданное число больше.";
+                }
+                else
+                {
+                    resultTextBlock.Text = "Загаданное число меньше.";
+                }
+
+                guessNumberTextBox.Text = "";
+            }
+            else
+            {
+                resultTextBlock.Text = "Введите число!";
+            }
+        }
+
+        private void guessButton_Click(object sender, RoutedEventArgs e)
+        {
+            HandleGuess();
+        }
+
+        private void guessNumberTextBox_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if (e.Key == System.Windows.Input.Key.Enter)
+            {
+                HandleGuess();
+            }
+        }
+    }
+}
---
+++ ugadaika/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ugadaika
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void StarGameButton_Click(object sender, RoutedEventArgs e)
+        {
+            Game g = new Game();
+            g.Show();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            AboutBox1 ab = new AboutBox1();
+            ab.Show();
+        }
+
+        private void ExitButton_Click_1(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void ChangeColor(object sender, RoutedEventArgs e)
+        {
+            string content = (sender as RadioButton).Content.ToString();
+            if (content == "Синий")
+            {
+                Background = Brushes.Blue;
+            }
+            else if (content == "Зеленый")
+            {
+                Background = Brushes.Green;
+            }
+            else
+                Background = Brushes.Pink;
+
+        }
+    }
+}
